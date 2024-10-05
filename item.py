a351b47019469fcc420879562fae29c76763f8b0
Nacho190214 <nachiket.bambhaniya@outlook.com>
2024-10-05 13:10:38 +0530

    Add files via upload

---
+++ item.py
@@ -0,0 +1,25 @@
+#elif sentenses
+item=input("enter the item code: ")
+
+if item== "1001":
+    print("Your item is Refrigerator")
+    print("COST:₹3400")
+elif item== "1002":
+    print("Your item is Smart TV")
+    print("COST:₹2500")
+elif item== "1003":
+    print("Your item is IPhone 16")
+    print("COST:₹1800")
+elif item== "1004":
+    print("Your item is a Monitor")
+    print("COST:₹1500")
+elif item== "1902" or "OVRALL" or "ovrall":
+    print("       TNC Electronics")
+    print("          Monitor")
+    print("         IPhone 16")
+    print("          Smart TV")
+    print("        Refrigerator")
+    print("Total cost:9200 ")
+    print("(THIS IS A COMPUTER GENERATED RECIPT)")
+    print("(NO SIGNATURE IS NEEDED)")
+input("Press enter to exit;")
